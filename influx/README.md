434d273475a8ac59cfa620e9544ecc82b58ed484
testbed <rp2025.comms@gmail.com>
2026-03-09 14:01:51 +0100

    update

--- influx/README.md
+++ influx/README.md
@@ -10,3 +10,22 @@ curl -i -X GET http://localhost:8086/health
 curl -i -X GET http://localhost:8086/api/v2/buckets \
   -H "Authorization: Token 93ukPnhw6Yrr38ZTd4Ihdj8BgSIVQhb9Xu5bVk3qf7teMdj66gQxuypZB81GhqKaxNn_a02l93437KAYJ6zkAw==" \
   -H "Content-Type: application/json"
+
+
+url = f"http://localhost:8086"
+token = "93ukPnhw6Yrr38ZTd4Ihdj8BgSIVQhb9Xu5bVk3qf7teMdj66gQxuypZB81GhqKaxNn_a02l93437KAYJ6zkAw=="
+bucket = "UEdata1"
+org = "my-org"
+measurement= "UEThroughput"
+time_range = "-1h"  
+
+
+curl --request POST \
+"http://localhost:8086/api/v2/query?org=my-org" \
+--header "Authorization: Token 93ukPnhw6Yrr38ZTd4Ihdj8BgSIVQhb9Xu5bVk3qf7teMdj66gQxuypZB81GhqKaxNn_a02l93437KAYJ6zkAw==" \
+--header "Accept: application/csv" \
+--header "Content-Type: application/vnd.flux" \
+--data 'from(bucket: "UEdata1") 
+          |> range(start: -1h) 
+          |> filter(fn: (r) => r._measurement == "UEThroughput") 
+          |> limit(n: 10)'
--- influx/query-data.py
+++ influx/query-data.py
@@ -1,33 +1,21 @@
-import influxdb_client_3 as influxdb
+from src.influx_db import query_data
+from src.constants import INFLUX_BUCKET, INFLUX_MEASUREMENT
+from src.helpers import load_config
+
+################################################################################
+# This script queries data from InfluxDB and prints the results to the console #
+################################################################################
 
 
-# 1. Configuration
-DATABASE = "bmw_sales"
-TOKEN = "apiv3_2A5PBZxv2vG0HDY5JAmmRkNI8DuLzRKodLDzVhcQeI0i-paCbCukwkwO3VgWBAmB-1egn5HZ5QdfDbc8a8yUVQ"
-HOST = "http://127.0.0.1:8181" # Update to your region
+config = load_config()
+url = f"http://localhost:{config['influxdb']['port']}"
+token = config['influxdb']['token']
+bucket = config['influxdb']['bucket']
+org = config['influxdb']['org']
+measurement= config['influxdb']['measurement']
+time_range = "-1h"   
 
-# 2. Initialize Client
-client = influxdb.InfluxDBClient3(
-    host=HOST,
-    token=TOKEN,
-    database=DATABASE
-)
-
-# 3. SQL Query
-query = 'SELECT * FROM "bmw_sales" LIMIT 5'
-
-try:
-    # Execute query - returns an Arrow Table
-    table = client.query(query=query, language="sql")
-    
-    # Convert the Arrow Table to a Pandas DataFrame
-    df = table.to_pandas()
-    
-    print("--- Head of 'bmw_sales' ---")
-    print(df)
-
-except Exception as e:
-    print(f"Query failed: {e}")
-
-finally:
-    client.close()
+print(f"Configuration: url={url}, bucket={bucket}, org={org}, measurement={measurement}, time_range={time_range}")
+if __name__ == '__main__':
+    print(f"Querying InfluxDB at {url}...")
+    query_data(measurement=measurement, bucket=bucket, url=url, token=token, org=org, limit=1, time_range=time_range)
